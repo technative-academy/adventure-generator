f2ddf328f432951a474f89109e1354e88773ac65
Matt Nightingale <matt.snail@gmail.com>
2025-10-09 19:47:20 +0100

    statscard js and some styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,13 @@
 import Tabs from './tabs.js'
 import cardTabs from './tabsCard.js'
 import JsonParser from './jsonParser.js'
+import statsTabs from './statsCard.js'
 
 const tabs = new Tabs('.nav', '.main')
 
 document.addEventListener('DOMContentLoaded', () => {
     cardTabs();
-    tabs();
+    statsTabs();
 })
 
 
