f404d5aa6d303887e749eb50d042954c451421cc
timtim40a <timtim40a@gmail.com>
2025-10-12 14:48:56 +0100

    path change, ratings js

--- scripts/rating.js
+++ scripts/rating.js
@@ -2,14 +2,14 @@ function drawRating(rating, div) {
     rating = Math.round(rating)
     for (let i = 0; i < rating; i += 1) {
         const star = document.createElement('img')
-        star.src = '../assets/star.svg'
+        star.src = 'assets/star.svg'
         star.classList.add('lit__star')
         star.classList.add('star')
         div.appendChild(star)
     }
     for (let i = 0; i < 5 - rating; i += 1) {
         const star = document.createElement('img')
-        star.src = '../assets/star.svg'
+        star.src = 'assets/star.svg'
         star.classList.add('star')
         div.appendChild(star)
     }
